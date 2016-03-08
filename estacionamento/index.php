d0bad52a3df7acb3756c0fe1f3af6fe7e7dda973
Alex Andrade <alexandradeneiva@gmail.com>
2016-03-07 22:05:03 -0300

    Estruturação da página inicial do projeto

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -1,1 +1,12 @@
-<?php
+<!doctype html>
+<html lang="pt - BR">
+<head>
+     <meta charset="UTF-8" />
+     <title>Página inicial do projeto</title>
+     <link rel="stylesheet" href="bootstrap/min.css">
+     </head>
+     <body>
+         <h1>Página inicial do projeto</h1>
+         <?php print "Impresso em PHP";?>
+     </body>
+     </html>
